fcbc7ac0b04747457d92e848c1f0a5a27e0215fc
Gaurav <kaushikgaurav08@gmail.com>
2018-12-24 19:54:11 -0500

    Code Cleanup - Remove temporary visited array

--- src/leetcode/medium/MaxAreaOfIsland.java
+++ src/leetcode/medium/MaxAreaOfIsland.java
@@ -1,8 +1,40 @@
 package leetcode.medium;
 
-import java.util.Arrays;
+/**
+@author :
+		Gaurav Kaushik
+		https://leetcode.com/kaushikgaurav08/
+		https://www.linkedin.com/in/gvk28/ 		
+        https://github.com/gauravkaushik
+========================================================================
+https://leetcode.com/problems/max-area-of-island/
+=========================================================================
+Given a non-empty 2D array grid of 0's and 1's, an island is a group of 1's (representing land) connected 4-directionally (horizontal or vertical.) You may assume all four edges of the grid are surrounded by water.
+
+Find the maximum area of an island in the given 2D array. (If there is no island, the maximum area is 0.)
+
+Example 1:
+
+[[0,0,1,0,0,0,0,1,0,0,0,0,0],
+ [0,0,0,0,0,0,0,1,1,1,0,0,0],
+ [0,1,1,0,1,0,0,0,0,0,0,0,0],
+ [0,1,0,0,1,1,0,0,1,0,1,0,0],
+ [0,1,0,0,1,1,0,0,1,1,1,0,0],
+ [0,0,0,0,0,0,0,0,0,0,1,0,0],
+ [0,0,0,0,0,0,0,1,1,1,0,0,0],
+ [0,0,0,0,0,0,0,1,1,0,0,0,0]]
+Given the above grid, return 6. Note the answer is not 11, because the island must be connected 4-directionally.
+Example 2:
+
+[[0,0,0,0,0,0,0,0]]
+Given the above grid, return 0.
+Note: The length of each dimension in the given grid does not exceed 50.
+ */
 
 public class MaxAreaOfIsland {
+    
+    int area = 0;
+    int maxArea = 0;
     public int maxAreaOfIsland(int[][] grid) {
         
         //do dfs from top left node of each island
@@ -14,53 +46,35 @@ public class MaxAreaOfIsland {
         
         int rows = grid.length;
         int cols = grid[0].length;
-        int maxArea = Integer.MIN_VALUE;
+        
         boolean[][] visited = new boolean[rows][cols];
-        boolean[][] visited_temp = new boolean[rows][cols];        
         
         for(int i=0; i<rows; i++)
         {
             for(int j=0; j<cols; j++)
             {
                 if(grid[i][j] == 1 && !visited[i][j])
-                {                    
-                    maxArea = Math.max(maxArea, findArea(i, j, rows, cols, grid, visited, visited_temp));
-                    visited[i][j] = true;
+                {
+                    area = 0;
+                    dfs(i, j, rows, cols, grid, visited);
                 }
             }
         }
         
-        return maxArea==Integer.MIN_VALUE?0:maxArea;        
+        return maxArea;        
     }
     
-    int findArea(int i, int j, int rows, int cols, int[][] grid, boolean[][] visited, boolean[][] visited_temp)
-    {       
-        for(boolean[] row : visited_temp)
-            Arrays.fill(row, false);
-        
-        dfs(i,j,rows,cols,grid,visited,visited_temp);
-        
-        int area = 0;
-        for(int x=0; x<rows; x++)
-        {
-            for(int y=0; y<cols; y++)
-            {
-                if(visited_temp[x][y])
-                    area++;
-            }
-        }
-        return area;
-    }
-    
-    void dfs(int i, int j, int rows, int cols, int[][] grid, boolean[][] visited, boolean[][] visited_temp)
+    void dfs(int i, int j, int rows, int cols, int[][] grid, boolean[][] visited)
     {
-        if(i<0 || i>=rows || j<0 || j>=cols || grid[i][j]==0 || visited_temp[i][j])
+        if(i<0 || i>=rows || j<0 || j>=cols || grid[i][j]==0 || visited[i][j])
             return;
         visited[i][j] = true;
-        visited_temp[i][j] = true;
-        dfs(i-1,j,rows,cols,grid,visited,visited_temp);
-        dfs(i+1,j,rows,cols,grid,visited,visited_temp);
-        dfs(i,j-1,rows,cols,grid,visited,visited_temp);
-        dfs(i,j+1,rows,cols,grid,visited,visited_temp);
+        area = area + 1;
+        maxArea = Math.max(area,maxArea);
+        
+        dfs(i-1,j,rows,cols,grid,visited);
+        dfs(i+1,j,rows,cols,grid,visited);
+        dfs(i,j-1,rows,cols,grid,visited);
+        dfs(i,j+1,rows,cols,grid,visited);
     }
 }
