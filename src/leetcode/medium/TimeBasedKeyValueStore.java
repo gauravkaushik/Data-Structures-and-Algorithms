421c7a012f698bd315543041938cda374db7a1fb
Gaurav <kaushikgaurav08@gmail.com>
2019-02-06 07:54:58 -0500

    Solved leetcode problem 981. Time Based Key-Value Store

---
+++ src/leetcode/medium/TimeBasedKeyValueStore.java
@@ -0,0 +1,125 @@
+package leetcode.medium;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
+
+/**
+@author :
+		Gaurav Kaushik
+		https://leetcode.com/kaushikgaurav08/
+		https://www.linkedin.com/in/gvk28/ 		
+        https://github.com/gauravkaushik
+        
+========================================================================
+https://leetcode.com/problems/time-based-key-value-store/
+========================================================================
+
+Create a timebased key-value store class TimeMap, that supports two operations.
+
+1. set(string key, string value, int timestamp)
+
+Stores the key and value, along with the given timestamp.
+2. get(string key, int timestamp)
+
+Returns a value such that set(key, value, timestamp_prev) was called previously, with timestamp_prev <= timestamp.
+If there are multiple such values, it returns the one with the largest timestamp_prev.
+If there are no values, it returns the empty string ("").
+ 
+
+Example 1:
+Input: inputs = ["TimeMap","set","get","get","set","get","get"], inputs = [[],["foo","bar",1],["foo",1],["foo",3],["foo","bar2",4],["foo",4],["foo",5]]
+Output: [null,null,"bar","bar",null,"bar2","bar2"]
+Explanation:   
+TimeMap kv;   
+kv.set("foo", "bar", 1); // store the key "foo" and value "bar" along with timestamp = 1   
+kv.get("foo", 1);  // output "bar"   
+kv.get("foo", 3); // output "bar" since there is no value corresponding to foo at timestamp 3 and timestamp 2, then the only value is at timestamp 1 ie "bar"   
+kv.set("foo", "bar2", 4);   
+kv.get("foo", 4); // output "bar2"   
+kv.get("foo", 5); //output "bar2"   
+
+Example 2:
+Input: inputs = ["TimeMap","set","set","get","get","get","get","get"], inputs = [[],["love","high",10],["love","low",20],["love",5],["love",10],["love",15],["love",20],["love",25]]
+Output: [null,null,null,"","high","high","low","low"]
+ 
+
+Note:
+
+All key/value strings are lowercase.
+All key/value strings have length in the range [1, 100]
+The timestamps for all TimeMap.set operations are strictly increasing.
+1 <= timestamp <= 10^7
+TimeMap.set and TimeMap.get functions will be called a total of 120000 times (combined) per test case.
+*/
+
+public class TimeBasedKeyValueStore {
+
+    Map<String, TreeMap<Integer, String>> mp;
+    /** Initialize your data structure here. */
+    public TimeBasedKeyValueStore() {
+        mp = new HashMap<>();
+    }
+    
+    public void set(String key, String value, int timestamp) {
+        TreeMap<Integer, String> tMap = null;
+        if(!mp.containsKey(key))
+            tMap = new TreeMap<>();
+        else
+            tMap = mp.get(key);
+        tMap.put(timestamp, value);
+        mp.put(key, tMap);
+    }
+    
+    public String get(String key, int timestamp) {
+        if(!mp.containsKey(key))
+            return "";
+        TreeMap<Integer, String> tMap = mp.get(key);
+        Map.Entry<Integer, String> entry = tMap.floorEntry(timestamp);
+        if(entry == null)
+            return "";
+        return entry.getValue();
+    }
+}
+
+/**
+ * Your TimeMap object will be instantiated and called as such:
+ * TimeMap obj = new TimeMap();
+ * obj.set(key,value,timestamp);
+ * String param_2 = obj.get(key,timestamp);
+ */
+ 
+ 
+/* A more efficient solution from : https://leetcode.com/problems/time-based-key-value-store/solution/
+import javafx.util.Pair;
+
+class TimeMap {
+    Map<String, List<Pair<Integer, String>>> M;
+
+    public TimeMap() {
+        M = new HashMap();
+    }
+
+    public void set(String key, String value, int timestamp) {
+        if (!M.containsKey(key))
+            M.put(key, new ArrayList<Pair<Integer, String>>());
+
+        M.get(key).add(new Pair(timestamp, value));
+    }
+
+    public String get(String key, int timestamp) {
+        if (!M.containsKey(key)) return "";
+
+        List<Pair<Integer, String>> A = M.get(key);
+        int i = Collections.binarySearch(A, new Pair<Integer, String>(timestamp, "{"),
+                (a, b) -> Integer.compare(a.getKey(), b.getKey()));
+
+        if (i >= 0)
+            return A.get(i).getValue();
+        else if (i == -1)
+            return "";
+        else
+            return A.get(-i-2).getValue();
+    }
+}
+*/
