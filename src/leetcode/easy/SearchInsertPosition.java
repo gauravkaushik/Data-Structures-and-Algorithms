fff9e6ebb052c0ec3f02675edf7f9a91b5818940
Gaurav <kaushikgaurav08@gmail.com>
2019-02-08 06:42:24 -0500

    Solved leetcode problem 35. Search Insert Position

---
+++ src/leetcode/easy/SearchInsertPosition.java
@@ -0,0 +1,93 @@
+package leetcode.easy;
+
+/**
+@author :
+		Gaurav Kaushik
+		https://leetcode.com/kaushikgaurav08/
+		https://www.linkedin.com/in/gvk28/ 		
+        https://github.com/gauravkaushik
+        
+========================================================================
+https://leetcode.com/problems/search-insert-position/
+========================================================================
+
+Given a sorted array and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+
+You may assume no duplicates in the array.
+
+Example 1:
+Input: [1,3,5,6], 5
+Output: 2
+
+Example 2:
+Input: [1,3,5,6], 2
+Output: 1
+
+Example 3:
+Input: [1,3,5,6], 7
+Output: 4
+
+Example 4:
+Input: [1,3,5,6], 0
+Output: 0
+*/
+
+public class SearchInsertPosition {
+    public int searchInsert(int[] nums, int target) {
+        if(nums == null || nums.length == 0)
+            return 0;
+        int low = 0;
+        int high = nums.length-1;
+        
+        int mid;
+        while(low <= high)
+        {
+            mid = low + (high-low)/2;
+            if(target == nums[mid])
+                return mid;
+            else if(target < nums[mid])
+            {
+                if(mid-1 >=0)
+                {
+                    if(target > nums[mid-1])
+                        return mid;
+                    else
+                        high = mid-1;
+                }
+                else
+                    return 0;
+            }
+            else
+            {
+                if(mid+1 <= nums.length-1)
+                {
+                    if(target < nums[mid+1])
+                        return mid+1;
+                    else
+                        low = mid+1;
+                }
+                else
+                    return nums.length;
+            }
+        }
+        
+        return -1;
+        
+        
+    }
+}
+
+/*
+A concise solution from : https://leetcode.com/problems/search-insert-position/discuss/15080/My-8-line-Java-solution
+
+public int searchInsert(int[] A, int target) {
+    int low = 0, high = A.length-1;
+    while(low<=high){
+        int mid = (low+high)/2;
+        if(A[mid] == target) return mid;
+        else if(A[mid] > target) high = mid-1;
+        else low = mid+1;
+    }
+    return low;
+}
+*/
